02425192d0a49fe7f83897d976eb193796ba7bd5
dguo <dguo@umail.ucsb.edu>
2021-01-30 14:13:27 -0800

    test database stuff with twilio

---
+++ run.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+# set up Flask
+app = Flask(__name__)
+
+# set up Twilio
+# might not need
+account_sid = 'ACa5ad00afdf14b5cc8ea01dfaf2a6d9ff'
+auth_token = 'e1caac81c5d7a47628d804ec7f06dc8e'
+
+# set up Firestore
+cred = credentials.Certificate('pk.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+@app.route("/", methods=['GET', 'POST'])
+def sms():
+    # receive message contents
+    message_content = request.values.get('Body').lower()
+
+    # maybe update database
+
+
+    # reply to message
+    resp = MessagingResponse()
+
+    if message_content == 'HELP':
+        resp_message = 'TODO: display list of pending tasks\n' + 'COMPLETED: display list of completed tasks\n' + 'Task Commands:\n' + 'Add Task: add [task name] [time interval]' + '\n' + 'Cancel Task: cancel [task name]' + '\n' + 'Complete Task: complete [task name]'
+
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run()
